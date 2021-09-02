4d828e4382dcf0cb65586d02ade639e76485cf14
Basit Javed Awan <basit.javed.awan@gmail.com>
2021-09-02 06:14:23 +0300

    Add files via upload
    Completed Lecture Implementing the Perceptron Neural Network with Python and uploaded Code

---
+++ Deep Learning 103/01-01-Perceptron_and.py
@@ -0,0 +1,19 @@
+from Preprocessing.perceptron import Perceptron
+import numpy as np
+
+# Construct the AND dataset
+X = np.array([[0,0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [0], [0], [1]])
+
+# define our perceptron and train it
+p = Perceptron(X.shape[1], alpha=0.1)
+p.fit(X, y, epochs=20)
+
+# now that our perceptron in trained we can evaluate it
+print('[INFO] Testing Perceptron...')
+
+# Loop over the data points
+for (x, target) in zip(X, y):
+    # make predictions and show result
+    pred = p.predict(x)
+    print(f'[INFO] data= {x}, Ground-Truth= {target[0]}, Pred= {pred}')
