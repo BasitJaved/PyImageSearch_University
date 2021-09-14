ca195111195652a7968e4052243f7c544c4421b7
Basit Javed Awan <basit.javed.awan@gmail.com>
2021-09-14 06:40:34 +0300

    Add files via upload
    shallownet model

---
+++ Deep Learning 105/conv/shallownet.py
@@ -0,0 +1,29 @@
+# Keras shallownet
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import backend as K
+
+class ShallowNet:
+    @staticmethod
+    def build(width, height, depth, classes):
+        # initialize the model along with input shape to be channel last
+        model = Sequential()
+        inputShape = (height, width, depth)
+
+        # if we are using channels first update the input shape
+        if K.image_data_format() == 'channels_first':
+            inputShape = (depth, height, width)
+
+        # define first and only CONV=>ReLU layer
+        model.add(Conv2D(32, (3, 3), padding='same', input_shape=inputShape))
+        model.add(Activation('relu'))
+
+        # softmax classifier
+        model.add(Flatten())
+        model.add(Dense(classes))
+        model.add(Activation('softmax'))
+
+        return model
