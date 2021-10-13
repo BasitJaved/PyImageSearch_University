6a0bf2025a33a3fc929b8acb1034cbd0793785a9
Basit Javed Awan <basit.javed.awan@gmail.com>
2021-10-13 06:32:48 +0300

    Add files via upload
    uploaded Config file under lecture Introduction to Hyperparameter Tuning

---
+++ Deep Learning 130/Preprocessing/config.py
@@ -0,0 +1,6 @@
+# specify the path of the dataset
+CSV_PATH = 'abalon_train.csv'
+
+# Specify Column names
+COLS = ['Length', 'Diameter', 'Height', 'Whole weight', 'Shucked weight', 'Viscera weight',
+        'Shell weight', 'Age']
