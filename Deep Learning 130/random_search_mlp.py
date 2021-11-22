49c8c890d04ec49966ca3a43d663d18d05982cc4
Basit Javed Awan <basit.javed.awan@gmail.com>
2021-11-22 07:03:58 +0300

    Add files via upload
    Completed lecture Using scikit-learn to Tune Deep Learning Model Hyperparameters and uploaded code

--- Deep Learning 130/random_search_mlp.py
+++ Deep Learning 130/random_search_mlp.py
@@ -25,3 +25,29 @@ learning_rate=[1e-2, 1e-3, 1e-4]
 dropout = [0.3, 0.4, 0.5]
 batch_size = [4, 8, 16, 32]
 epochs = [10, 20, 30, 40]
+
+# create a dictionary from the hyperparameter grid
+grid = dict(
+    hidden_layer_one=hidden_layer_one,
+    learning_rate=learning_rate,
+    hidden_layer_two=hidden_layer_two,
+    dropout=dropout,
+    batch_size=batch_size,
+    epochs=epochs
+)
+
+# initialize a random search with a 3 fold cross-validation and then start the hyperparameter search process
+print('[INFO] performing random search...')
+searcher = RandomizedSearchCV(estimator=model, n_jobs=-1, cv=3, param_distributions=grid, scoring='accuracy')
+search_results = searcher.fit(trainX, trainY)
+
+# summarize grid search information
+best_score = search_results.best_score_
+best_params = search_results.best_params_
+print(f'[INFO] best score is {best_score} using {best_params}')
+
+# extract best model, make predictions on data and show a classification report
+print('[INFO] evaluating best model')
+best_model = search_results.best_estimator_
+accuracy = best_model.score(testX, testY)
+print(f'Accuracy: {accuracy*100}')
